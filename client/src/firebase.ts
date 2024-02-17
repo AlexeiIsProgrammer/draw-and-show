42a208dca2ec7086b673b4b00d36625a70a102bc
Alexei <avenge.now.r@mail.ru>
2024-02-17 23:50:13 +0300

    feat: add server, forgot about that

---
+++ client/src/firebase.ts
@@ -0,0 +1,13 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDHhQAIlvXvMWSTcecW4um1UXwFB_NNSYE',
+  authDomain: 'real-time-chatting-project.firebaseapp.com',
+  projectId: 'real-time-chatting-project',
+  storageBucket: 'real-time-chatting-project.appspot.com',
+  messagingSenderId: '687884914798',
+  appId: '1:687884914798:web:00fc2ffe5fee32fecf167f',
+};
+
+const app = initializeApp(firebaseConfig);
