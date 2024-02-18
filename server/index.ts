5232285a2832eaea1fef9b16ed2767b3a51d4ca5
Alexei <avenge.now.r@mail.ru>
2024-02-18 10:16:13 +0300

    feat: add dotenv and front link variable

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,9 @@ import express from "express";
 import {createServer} from "http";
 import {Server} from "socket.io";
 import {CircleType, LineType, RectangleType} from "./types";
+import {config} from "dotenv";
+
+config();
 
 const PORT = process.env.PORT || 4000;
 
@@ -9,7 +12,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: [process.env.FRONTEND_URL || "http://localhost:5173"],
   },
 });
 
